8394d811a4ab300fd23f07bd32495394ec491d21
kulakovskyi <torchok9@gmail.com>
2022-11-08 16:43:28 +0200

    add media and any ifo

--- front/js/main.js
+++ front/js/main.js
@@ -10,23 +10,12 @@ const playBtn = document.querySelector('.bonus__main-wheel-btn'),
       popupFirst = document.querySelector('.bonus__firstWin'),
       popupFirstBtn = document.querySelector('.bonus__firstWin-btn'),
       popupSecond = document.querySelector('.bonus__secondWin'),
-      rules = document.querySelector('.bonus__rules'),
-      footerRulesBtn = document.querySelector('.bonus__footer-rules'),
       overflow = document.querySelector('body'),
-      popupClose = document.querySelector('.bonus__rules-close'),
       video = document.querySelector('.bonus__main-video-bg'),
       wrapper = document.querySelector('.bonus')
 
 
 const bonusText = document.querySelector('.bonus__main-bubble');
-const bonusLink = document.querySelector('.bonus__secondWin-btn');
-const audioMain = document.querySelector('.main-audio');
-const audioWheel = document.querySelector('.wheel-audio');
-const audioPopup = document.querySelector('.popup-audio');
-const clickAudio = document.querySelector('.click-audio');
-audioMain.volume = 0.3;
-audioWheel.volume = 0.2;
-clickAudio.volume = 0.2;
 
 window.onload = videoSource(video, 'img/video.mp4', 'video/mp4');
 
@@ -42,35 +31,10 @@ function videoSource(element, src, type) {
 
 let triesCounter = 0
 
-footerRulesBtn.addEventListener('click', () => {
-    clickAudio.play()
-    overlay.classList.remove('opacity-overlay')
-    rules.classList.remove('hide')
-    main.classList.add('blur')
-    chips.forEach(function (el) {
-        el.classList.add('blur')
-    })
-    popupChips.forEach(function (el) {
-        el.classList.add('hide')
-    })
-})
-
-popupClose.addEventListener('click', () => {
-    clickAudio.play()
-    main.classList.remove('blur')
-    overlay.classList.add('opacity-overlay')
-    rules.classList.add('hide')
-    overflow.style.overflow = 'unset'
-    chips.forEach(function (el) {
-        el.classList.remove('blur')
-    })
-})
-
 playBtn.addEventListener('click', () => {
 
     if (triesCounter === 0) {
         runFirstRotation()
-        audioMain.play()
 
     } else {
         runSecondRotation()
@@ -82,7 +46,6 @@ function runFirstRotation() {
     playBtn.classList.remove('pulse-btn')
     playBtn.style.cursor = 'default'
     wrapper.style.pointerEvents = 'none'
-    audioWheel.play()
     setTimeout(() => {
         doAfterFirstRotation()
     }, 6000)
@@ -98,9 +61,6 @@ function doAfterFirstRotation() {
     displayPopup(popupFirst)
     wrapper.style.pointerEvents = 'auto'
     overflow.style.overflow = 'hidden'
-
-    audioWheel.pause()
-    audioPopup.play()
     setTimeout(() => {
         playBtn.classList.add('pulse-btn')
         playBtn.style.cursor = 'pointer'
@@ -114,7 +74,6 @@ function runSecondRotation() {
     playBtn.style.cursor = 'default'
     overflow.style.overflow = 'hidden'
     wrapper.style.pointerEvents = 'none'
-    audioWheel.play()
     setTimeout(() => {
         doAfterSecondRotation()
     }, 6000)
@@ -127,16 +86,10 @@ function doAfterSecondRotation() {
     secondWoman.classList.remove('hide')
     displayPopup(popupSecond)
     wrapper.style.pointerEvents = 'auto'
-    audioWheel.pause()
-    audioPopup.play()
 }
 
-bonusLink.addEventListener('click', () => {
-    clickAudio.play();
-})
 
 popupFirstBtn.addEventListener('click', () => {
-    clickAudio.play()
     main.classList.remove('blur')
     firstWoman.classList.add('hide')
     chips.forEach(function (el) {
